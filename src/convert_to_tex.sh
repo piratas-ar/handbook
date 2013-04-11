7168004394dd25c0a7a7dd2989f34bb2dca22c36
gnu_tesla <gnu_tesla@riseup.net>
2013-04-11 17:07:26 -0300

    Edición de la versión 1 (v1) de la traducción al español

--- src/convert_to_tex.sh
+++ src/convert_to_tex.sh
@@ -10,22 +10,27 @@ if [ ! -d $DIR ] ; then
 fi
 
 cat > $DIR/main.tex <<EOF
-\documentclass[oribibl]{scrbook}
-
+\documentclass[oribibl,oneside,pdftext]{scrbook}
 \usepackage{amsmath,amssymb,latexsym}
 \usepackage{algorithm, algorithmic}
 \usepackage{graphicx}
+\setkeys{Gin}{width=0.70\textwidth}
 \usepackage{varioref}
-\usepackage{hyperref}
 \usepackage{paralist}
 \usepackage{eurosym}
-
-
+\usepackage[spanish,activeacute]{babel}
+\usepackage[utf8]{inputenc}
+\usepackage[pdftex,bookmarks,colorlinks]{hyperref}
+%\oddsidemargin 0in
+%\textwidth 6.75in
+%\topmargin 0.2in
+%\headheight 0.2in
+%\textheight 8.5in
 
 \begin{document}
 
-\title{The Cryptoparty Handbook}
-\author{Various authors}
+\title{Manual de Cryptoparty}
+\author{Traducción del Partido Pirata de Argentina}
 
 \maketitle
 \tableofcontents
@@ -33,12 +38,12 @@ cat > $DIR/main.tex <<EOF
 EOF
 
 
-for d in capítulo*; do
+for d in capitulo*; do
 	if [ ! -d $DIR/$d ] ; then
 		mkdir $DIR/$d
 	fi
 	echo "\\graphicspath{{./$d/}}" >> $DIR/main.tex
-	title=`echo $d | sed 's/chapter_[0-9][0-9]_//; s/_/ /g; s/^./\U&/; s/ ./\U&/g'`
+	title=`echo $d | sed 's/capitulo_[0-9][0-9]_//; s/_/ /g; s/^./\U&/; s/ ./\U&/g'`
 	echo "\\chapter{$title}" >> $DIR/main.tex
 	for f in $d/*.md; do 
 		pandoc -f markdown -t latex $f -o $DIR/$f.tex
